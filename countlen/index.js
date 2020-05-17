0bba2673c63503ed37ca47c1f102ef7a97dc47d8
Preetam3620 <preetamamaske1@gmail.com>
2020-05-17 16:10:18 +0530

    typos cleared

--- countlen/index.js
+++ countlen/index.js
@@ -3,18 +3,15 @@
 // ordering of numbers.
 // --- Examples
 //   countlen("") === 0
-//   reverseInt("github") === 6
-//   reverseInt("hello") === 5
-
+//   countlen("github") === 6
+//   countlen("hello") === 5
 
 function countlen(str) {
+	let cnt = 0;
 
-    let cnt = 0;
+	while (str[cnt] != undefined) cnt++;
 
-    while(str[cnt] != undefined)
-        cnt++;
-
-    return parseInt(cnt);
+	return parseInt(cnt);
 }
 
 module.exports = countlen;
